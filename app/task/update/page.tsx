c178e3b67a017a1db4af6a659869f090f8617a4f
Chai Tze Chye <>
2024-07-29 20:02:58 +0800

    Fixed build error

--- app/task/update/page.tsx
+++ app/task/update/page.tsx
@@ -1,9 +1,12 @@
 import UpdateForm from "../components/updateForm";
+import { Suspense } from "react";
 
 export default function UpdateTaskPage() {
   return (
     <div className="max-w-md mx-auto my-5">
-      <UpdateForm />
+      <Suspense>
+        <UpdateForm />
+      </Suspense>
     </div>
   );
 }
